4b63ec123e2f990d119534c0f3c1e07a629e17ad
Samuel Xavier <sancrisxa@gmail.com>
2024-07-14 14:26:21 -0300

    Criar as entities

---
+++ src/main/java/com/sancrisxa/spring_security/entities/Role.java
@@ -0,0 +1,46 @@
+package com.sancrisxa.spring_security.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values {
+        BASIC(2L), ADM(1L);
+
+        long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+        public long getRoleId() {
+            return roleId;
+        }
+    }
+}
