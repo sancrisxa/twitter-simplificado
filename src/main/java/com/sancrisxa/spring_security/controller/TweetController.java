924c4b40766cd74b0675e9cedc3d091940d7251b
Samuel Xavier <sancrisxa@gmail.com>
2024-07-30 20:20:58 -0300

    Criar a funcionalidade de deletar tweets

--- src/main/java/com/sancrisxa/spring_security/controller/TweetController.java
+++ src/main/java/com/sancrisxa/spring_security/controller/TweetController.java
@@ -2,14 +2,15 @@ package com.sancrisxa.spring_security.controller;
 
 
 import com.sancrisxa.spring_security.controller.dto.CreateTweetDto;
+import com.sancrisxa.spring_security.entities.Role;
 import com.sancrisxa.spring_security.entities.Tweet;
 import com.sancrisxa.spring_security.repository.TweetRepository;
 import com.sancrisxa.spring_security.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -39,5 +40,27 @@ public class TweetController {
 
         return ResponseEntity.ok().build();
 
+    }
+
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId, JwtAuthenticationToken token) {
+
+        var tweet = tweetRepository.findById(tweetId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var isAdmin = user.get().getRoles().stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+
+
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(tweetId);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok().build();
     }
 }
